daf5eb65e61392ee0ccc06bf2362a94689bda9eb
Victor Onyango <105198927+VitomsCatena@users.noreply.github.com>
2026-02-17 13:13:26 +0300

    Add menu items for drinks and starters

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-
+<?php
+$menu = [
+    "Drinks" => [
+        ["name" => "Shaah Cadeys", "price" => 50], ["name" => "Shaah Bigays", "price" => 40],
+        ["name" => "Camel Milk", "price" => 150], ["name" => "Mango Puree", "price" => 120],
+        ["name" => "Vimto Float", "price" => 150], ["name" => "Isbaandhees Mix", "price" => 120],
+        ["name" => "Lemon Mint Crush", "price" => 150], ["name" => "Avocado Shake", "price" => 180],
+        ["name" => "Stoney Tangawizi", "price" => 70], ["name" => "Tamarind (Bari)", "price" => 100]
+    ],
+    "Starters" => [
+        ["name" => "Beef Sambusa", "price" => 30], ["name" => "Chicken Sambusa", "price" => 35],
+        ["name" => "Bajiya", "price" => 50], ["name" => "Nafaqo", "price" => 60],
+        ["name" => "Mushakal", "price" => 150], ["name" => "Kashata", "price" => 40],
+        ["name" => "Salami Rolls", "price" => 120], ["name" => "Fish Cutlets", "price" => 100],
+        ["name" => "Lentil Soup", "price" => 150], ["name" => "Veg Spring Rolls", "price" => 30]
+    ],
