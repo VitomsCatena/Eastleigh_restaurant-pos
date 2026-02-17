3f70f079304e43a75cdda53aa6566ca2c0e15b88
Victor Onyango <105198927+VitomsCatena@users.noreply.github.com>
2026-02-17 13:14:26 +0300

    Add main dishes, platters, snacks, and desserts

--- index.php
+++ index.php
@@ -14,3 +14,33 @@ $menu = [
         ["name" => "Salami Rolls", "price" => 120], ["name" => "Fish Cutlets", "price" => 100],
         ["name" => "Lentil Soup", "price" => 150], ["name" => "Veg Spring Rolls", "price" => 30]
     ],
+    "Main Dish" => [
+        ["name" => "Bariis Iskukaris", "price" => 450], ["name" => "Beef Suqaar", "price" => 400],
+        ["name" => "Chicken Suqaar", "price" => 380], ["name" => "Goat Meat", "price" => 550],
+        ["name" => "Pasto Salatade", "price" => 350], ["name" => "Fish Steak", "price" => 600],
+        ["name" => "KK (Kibeiti)", "price" => 350], ["name" => "Camel Meat", "price" => 650],
+        ["name" => "Muufo & Stew", "price" => 400], ["name" => "Soor & Digir", "price" => 300]
+    ],
+    "Platters" => [
+        ["name" => "6th Street Special", "price" => 1200], ["name" => "Camel Lovers", "price" => 1500],
+        ["name" => "Seafood Platter", "price" => 1800], ["name" => "Vegetarian Feast", "price" => 800],
+        ["name" => "Sultan's Tray", "price" => 3500], ["name" => "Breakfast Platter", "price" => 600],
+        ["name" => "KK Mountain", "price" => 1200], ["name" => "Meat Lovers Combo", "price" => 1600],
+        ["name" => "Shared Pasto", "price" => 1000], ["name" => "Eastleigh Mix", "price" => 1400]
+    ],
+    "Snacks" => [
+        ["name" => "Malawah", "price" => 50], ["name" => "Anjero", "price" => 40],
+        ["name" => "Kabaab", "price" => 100], ["name" => "Bur (Mandazi)", "price" => 20],
+        ["name" => "Grilled Maize", "price" => 50], ["name" => "Samosa Wraps", "price" => 150],
+        ["name" => "Potato Wedges", "price" => 150], ["name" => "Doolsho", "price" => 80],
+        ["name" => "Bisbaas Bread", "price" => 60], ["name" => "Fried Cassava", "price" => 70]
+    ],
+    "Desserts" => [
+        ["name" => "Xalwo", "price" => 100], ["name" => "Gashaato", "price" => 50],
+        ["name" => "Cream Caramel", "price" => 200], ["name" => "Icun", "price" => 150],
+        ["name" => "Labani", "price" => 250], ["name" => "Fruit Salad", "price" => 150],
+        ["name" => "Baklava", "price" => 300], ["name" => "Sweet Surbiyaan", "price" => 250],
+        ["name" => "Zalabia", "price" => 100], ["name" => "Shushumow", "price" => 120]
+    ]
+];
+?>
